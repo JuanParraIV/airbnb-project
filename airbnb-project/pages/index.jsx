79d994c7c96b57b38966f8c8d96c8da15bac7793
JuanParraIV <jmparra1993@gmail.com>
2022-06-03 18:14:23 -0500

    finish small card component in main tag

--- airbnb-project/pages/index.jsx
+++ airbnb-project/pages/index.jsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
-const Home = ({exploteData}) => {
+const Home = ({ exploteData }) => {
   console.log(exploteData);
   return (
     <div className="">
@@ -14,41 +15,37 @@ const Home = ({exploteData}) => {
       <Header />
       <Banner />
 
-      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
-        <section className='pt-6'>
-          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           {/*Pull some data from a server - API endpoints */}
-          {exploteData && exploteData.map((item, index) =>(
-              <div className="flex flex-row">
-                <div className="flex">
-                  <img src={item.img} alt="" />
-                </div>
-                <div className="">
-                  <h3 className="text-2xl font-semibold">{item.location}</h3>
-                  <p className="text-lg">{item.distance}</p>
-
-                </div>
-              </div>
-            )
-          )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploteData &&
+              exploteData?.map(({ img, location, distance }, index) => (
+                <SmallCard
+                  key={index}
+                  img={img}
+                  location={location}
+                  distance={distance}
+                />
+              ))}
+          </div>
         </section>
       </main>
-
     </div>
-  )
-}
+  );
+};
 
 export const getStaticProps = async () => {
-  const exploteData = await fetch(
-    'https://links.papareact.com/pyp')
-    .then(
-      res => res.json());
+  const exploteData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
 
   return {
     props: {
-      exploteData
-    }
-  }
-}
-export default Home
+      exploteData,
+    },
+  };
+};
+export default Home;
