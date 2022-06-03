5245c1aef20ec3c7a72a40dcda33053fcea41544
JuanParraIV <jmparra1993@gmail.com>
2022-06-03 17:40:57 -0500

    implementing ssr

--- airbnb-project/pages/index.jsx
+++ airbnb-project/pages/index.jsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
-const Home = () => {
+const Home = ({exploteData}) => {
+  console.log(exploteData);
   return (
     <div className="">
       <Head>
@@ -18,6 +19,19 @@ const Home = () => {
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
           {/*Pull some data from a server - API endpoints */}
+          {exploteData && exploteData.map((item, index) =>(
+              <div className="flex flex-row">
+                <div className="flex">
+                  <img src={item.img} alt="" />
+                </div>
+                <div className="">
+                  <h3 className="text-2xl font-semibold">{item.location}</h3>
+                  <p className="text-lg">{item.distance}</p>
+
+                </div>
+              </div>
+            )
+          )}
         </section>
       </main>
 
@@ -25,4 +39,16 @@ const Home = () => {
   )
 }
 
+export const getStaticProps = async () => {
+  const exploteData = await fetch(
+    'https://links.papareact.com/pyp')
+    .then(
+      res => res.json());
+
+  return {
+    props: {
+      exploteData
+    }
+  }
+}
 export default Home
